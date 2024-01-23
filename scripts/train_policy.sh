1297299563fe96a9a917cb00ed9237bdbfc44848
TristanSaidi <tls2160@columbia.edu>
2024-01-23 19:45:03 +0000

    repo cleanup (removed legacy things irrelevant to handem

--- scripts/train_policy.sh
+++ scripts/train_policy.sh
@@ -2,17 +2,16 @@
 GPUS=$1
 SEED=$2
 EXP_NAME=$3
-TASK=$4
 
 array=( $@ )
 len=${#array[@]}
-EXTRA_ARGS=${array[@]:5:$len}
+EXTRA_ARGS=${array[@]:4:$len}
 EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
 
 echo extra "${EXTRA_ARGS}"
 
 CUDA_VISIBLE_DEVICES=${GPUS} \
-python train.py task=${TASK} headless=True seed=${SEED} \
+python train.py task=HANDEM headless=True seed=${SEED} \
 train.algo=HANDEM \
 train.handem.output_name="${EXP_NAME}" \
 ${EXTRA_ARGS}
